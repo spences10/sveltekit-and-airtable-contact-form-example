2fb666766018d56771d9b35958c1fbdfa00b9b1f
spences10 <spences10apps@gmail.com>
2023-04-30 20:04:42 +0100

    refactor: :recycle: move pages out to own routes
    add style and layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../styles.css'
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+  main {
+    margin: 0 auto;
+    padding: 1rem;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
-<script>
-	import ContactForm from '$lib/contact-form.svelte'
-</script>
-
 <h1>SvelteKit contact form submission with Airtable</h1>
 
-<ContactForm />
+<p>Two ways to submit form data.</p>
+
+<p>
+	With an
+	<a href="/event-handler">Event handler</a>
+</p>
+
+<p>
+	With an
+	<a href="/action">Action</a>
+</p>
